04379f14dd7b9a42e61f64c8b1b306f30c61f9b6
torcuata22 <marilynjmarquez@gmail.com>
2023-07-03 12:13:52 -0400

    second

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,16 @@
+class RoomsController < ApplicationController
+    before_action :set_room, only: %i[show]
+
+    def index 
+        @rooms = Room.all
+    end
+
+    def show
+    end
+
+    private
+    def set_room
+        @room = Room.find(params[:id])
+    end
+
+end
